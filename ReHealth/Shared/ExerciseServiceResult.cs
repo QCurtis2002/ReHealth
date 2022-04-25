2656edab7dcf983a2ab838d8bdf43dc1a629fb16
Quinn Curtis <quinn.curtis@borwell.com>
2022-04-25 15:05:14 +0100

    15:05 commit

---
+++ ReHealth/Shared/ExerciseServiceResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReHealth.Shared
+{
+    public class ExerciseServiceResult
+    {
+        public List<Exercise> Exercises { get; set; }
+        public int pages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
